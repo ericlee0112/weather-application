82a70bb731572751e4d6105def8ea6e3b96647a9
Eric Lee <ericjlee@hotmail.com>
2017-08-11 17:12:46 -0400

    Create weather.js
    basic backend that contains the http api that fetches data and outputs them back to the .html file.

---
+++ weather.js
@@ -0,0 +1,18 @@
+ var api = "http://api.wunderground.com/api/898609db2ff0c335/geolookup/conditions/q/";
+ api = api + 
+    $(document).ready( function(){
+  $.ajax({
+  type:"GET",
+  url : api,
+  dataType : "json",
+  success : function(parsed_json) {
+  var location = parsed_json.location.city;
+  var temp = parsed_json.current_observation.temp_c;
+  var feelsLike = parsed_json.current_observation.feelslike_c;
+  
+  document.getElementById("location").innerHTML = "location : " +  location;
+  document.getElementById("temp").innerHTML = "temperature(celsius) : " + temp;
+  document.getElementById("feelsLike").innerHTML = "feels more like : " + feelsLike; 
+}//end of success function
+  });//end of ajax
+});//end of jquery
