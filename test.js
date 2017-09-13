76d39f18ec76afadc8e5a68c8caee2e533905fe9
Eric Lee <ericjlee@hotmail.com>
2017-09-13 00:05:06 -0400

    Update test.js
    to show user when data was last updated

--- test.js
+++ test.js
@@ -10,30 +10,36 @@ var posLat, posLon, api;
 
 $.getJSON(api, function(parsed_json){
   var tempK = parsed_json.main.temp;
-  var tempC = Math.round((tempK - 273.15)*10)/10;
-  var tempF = tempK*(9/5) - 459.67;
-  var tempSwap = true;
-
+  var tempC = Math.round((tempK - 273.15)*10)/10;//to round to the nearest decmial place
+  var tempF = Math.round((tempK*(9/5) - 459.67)*10)/10;//to convert from kelvin to farenheit and then to round to nearest decimal
+  var tempSwap = true;//to set tempswap boolean false so that user can alternate between celsius and farenheit
+  
   //console.log(name);
   //console.log(longtitude);
   document.getElementById("longtitude").innerHTML = "longtitude: " + parsed_json.coord.lon;
   document.getElementById("latitude").innerHTML = "latitude : " + parsed_json.coord.lat;
   document.getElementById("location").innerHTML = "location : " + parsed_json.name; 
-  document.getElementById("temp").innerHTML = "celsius : " + tempC;
+  document.getElementById("temp").innerHTML = "celsius : " + tempC + " &#8451;";//to display the celsius symbol
   document.getElementById("condition").innerHTML = "weather conditions : " + parsed_json.weather[0].description;
+  document.getElementById("wind").innerHTML = "wind speed : " + parsed_json.wind.speed;//display windspeed 
 
-  $("#temp").click(function(){
+  $("#temp").click(function(){//if user clicks on temp button
     if(tempSwap === false){
-      document.getElementById("temp").innerHTML = "farenheit : " + tempF;
+      document.getElementById("temp").innerHTML = "farenheit : " + tempF + " &#8457;";//to display the farenheit symbol
       tempSwap = true;
     }
     else{
-      document.getElementById("temp").innerHTML = "celsius : " + tempC;
+      document.getElementById("temp").innerHTML = "celsius : " + tempC + " &#8451;";
       tempSwap = false;
     }
   });
 
-});
+var d = new Date();
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+document.getElementById("date").innerHTML = days[d.getDay()];// to show user when data was last updated
+
+
+});//end of getJSON
 
   });
      }
